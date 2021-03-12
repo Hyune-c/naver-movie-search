262f38f8ca7946baa710e2c4c2acbdda4ea9930d
choibyunghyeon <choi8608@gmail.com>
2021-03-13 04:17:33 +0900

    Initial commit

---
+++ src/main/java/com/project/navermoviesearch/NaverMovieSearchApplication.java
@@ -0,0 +1,13 @@
+package com.project.navermoviesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverMovieSearchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NaverMovieSearchApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/project/navermoviesearch/NaverMovieSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.navermoviesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverMovieSearchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
