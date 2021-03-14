b9263ce2ecbc1142d826537de17c2abfe5342155
choibyunghyeon <choi8608@gmail.com>
2021-03-14 13:24:10 +0900

    Issue-2 🔧 DB, profile 설정 추가

---
+++ src/main/java/com/project/navermoviesearch/config/JpaListenerConfig.java
@@ -0,0 +1,9 @@
+package com.project.navermoviesearch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaListenerConfig {
+}
--- src/test/java/com/project/navermoviesearch/config/TestContextInitializer.java
+++ src/test/java/com/project/navermoviesearch/config/TestContextInitializer.java
@@ -11,8 +11,11 @@ public class TestContextInitializer implements ApplicationContextInitializer<Con
   public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
     // 공개 repository 이기에 삭제하였습니다.
     // 테스트 케이스를 돌리시려면 Key 정보를 채워주세요.
-    setEnv("NAVER_CLIENT_ID", "xxxxxxxxxxx");
-    setEnv("NAVER_CLIENT_SECRET", "xxxxxxxx");
+    setEnv("NAVER_CLIENT_ID", "");
+    setEnv("NAVER_CLIENT_SECRET", "");
+    setEnv("JDBC_URL", "");
+    setEnv("JDBC_USERNAME", "");
+    setEnv("JDBC_PASSWORD", "");
   }
 
   private static void setEnv(String key, String value) {
